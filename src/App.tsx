2d7bdfca0f1e15c779b650af64976a494fd0568a
Saurabh Sood <saurabhsood91@gmail.com>
2020-05-31 13:18:00 -0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import configureStore from './state/store';
+import Header from './components/Header';
 
 function App() {
     return (
         <Provider store={configureStore()}>
-            <div className="App"></div>
+            <div className="App">
+                <Header />
+            </div>
         </Provider>
     );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import './index.css';
+
+const Header: React.FunctionComponent = () => <h1 className="App-title">Be nice to the duck! :)</h1>;
+
+export default Header;
