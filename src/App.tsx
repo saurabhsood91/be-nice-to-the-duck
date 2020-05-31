065a3672390d54712c4a0e4b8c1184b74077ec7e
Saurabh Sood <saurabhsood91@gmail.com>
2020-05-31 12:18:34 -0700

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 import configureStore from './state/store';
@@ -7,17 +6,7 @@ import configureStore from './state/store';
 function App() {
     return (
         <Provider store={configureStore()}>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <div className="App"></div>
         </Provider>
     );
 }
