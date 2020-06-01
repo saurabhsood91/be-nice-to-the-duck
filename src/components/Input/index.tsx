812aa210cfc484ffa0912c781585720ca6cc15f5
Saurabh Sood <saurabhsood91@gmail.com>
2020-05-31 18:01:40 -0700

    wire up actions

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,24 +1,42 @@
 import * as React from 'react';
 import './index.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { textEntered } from '../../state/app/actions';
 
 const InputBox: React.FunctionComponent = () => {
-    const inputRef = useRef<HTMLInputElement | null>(null);
     const dispatch = useDispatch();
+    const [text, setText] = useState('');
+    const inputElement = useRef(null);
 
-    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        const value = inputRef.current?.value;
-        if (!value) {
+    const onChange = () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const enteredText = inputElement.current?.value;
+        if (enteredText === '') {
+            setText('');
             return;
         }
+        console.log({ enteredText });
+        setText(enteredText);
+    };
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.keyCode === 13) {
-            dispatch(textEntered({ value }));
+            dispatch(textEntered({ value: text }));
         }
     };
 
-    return <input type="text" className="Input-box" ref={inputRef} onKeyUp={handleKeyPress} />;
+    return (
+        <input
+            type="text"
+            className="Input-box"
+            ref={inputElement}
+            onChange={onChange}
+            value={text}
+            onKeyDown={onKeyDown}
+        />
+    );
 };
 
 export default InputBox;
